3aa3167452dae5ba8bb92e481c3ee552f41c8b06
noturname <chmielewskij1@gmail.com>
2016-05-05 23:15:55 +0000

    added signup btns

--- db/migrate/20160505224722_create_plans.rb
+++ db/migrate/20160505224722_create_plans.rb
@@ -1,6 +1,6 @@
 class CreatePlans < ActiveRecord::Migration
   def change
-    create_table :plans do |t|
+    create_table :test2 do |t|
       t.string :name
       t.decimal :price
       t.timestamps
